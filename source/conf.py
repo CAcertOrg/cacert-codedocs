a5bb7e680bcc1950f70c13e37bab2b3e4d5639b1
Jan Dittberner <jandd@cacert.org>
2018-10-26 23:48:01 +0200

    Add CAcert customization

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -69,7 +69,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -77,13 +77,21 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'sidebarbgcolor': '#f5f7f7',
+    'sidebartextcolor': '#334d55',
+    'sidebarlinkcolor': '#005fa9',
+}
+
+html_logo = os.path.join('images', 'CAcert-logo-colour.svg')
+
+html_favicon = os.path.join('images', 'favicon.ico')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
